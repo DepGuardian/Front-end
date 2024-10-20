dda8ef003ad7fe5574d495bd4f28e6409c5656f9
Lando Fabrizio Veramendi Hilario <103605414+Landov311@users.noreply.github.com>
2024-10-20 15:50:57 -0500

    Add files via upload

--- deep-guardian/App.js
+++ deep-guardian/App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { useFonts } from "expo-font"; 
+import * as SplashScreen from "expo-splash-screen";
 import Landing from "./components/Landing";
 import SignIn from "./components/SignIn";
-
+import Reservas from "./components/Reservas";
 const Stack = createNativeStackNavigator();
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync(); 
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
@@ -22,12 +45,22 @@ export default function App() {
           component={SignIn}
           options={{ headerShown: false }}
         />
+        <Stack.Screen
+          name="Reservas"
+          component={Reservas}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
